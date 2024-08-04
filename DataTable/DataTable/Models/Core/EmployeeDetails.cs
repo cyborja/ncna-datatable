9fe80c0cf8f1f405f4b5a669446fa19ea6ffe656
cyborja <cyborj88@gmail.com>
2024-08-04 14:48:27 +0800

    add model

---
+++ DataTable/DataTable/Models/Core/EmployeeDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace DataTable.Models.Core
+{
+    public class EmployeeDetails
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Position { get; set; }
+        public string Office { get; set; }
+        public int Age { get; set; }
+        public DateOnly StartDate { get; set; }
+        public double Salary { get; set; }
+        public string Extension { get; set; }
+        public string Email { get; set; }
+    }
+}
